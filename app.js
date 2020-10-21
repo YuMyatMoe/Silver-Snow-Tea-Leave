8bad852ed6104df1aa00d8bd5331d0532316ad03
Yu Myat Moe <yumyatmoe07@gmail.com>
2020-10-21 11:02:37 -0700

    test

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ let actionKeyboard = {
                 "BgMedia": "http://www.url.by/test.gif",
                 "BgLoop": true,
                 "ActionType": "reply",
-                "ActionBody": "reply",               
+                "ActionBody": "my-stock",               
                 "Text": "My Stock",
                 "TextVAlign": "middle",
                 "TextHAlign": "center",
